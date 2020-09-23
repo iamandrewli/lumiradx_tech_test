45c04a189d94195d2266b36e8d4d58dbb9ca8ade
Andrew Li <AndrewLi@Andrews-MacBook-Pro.local>
2020-09-23 16:18:18 +0100

    reworked test to add function for count

--- automated_integration_tests/post.py
+++ automated_integration_tests/post.py
@@ -6,18 +6,21 @@ HEADERS = {'Content-Type': 'application/json'}
 GET_ENDPOINT = "http://localhost:8888/api/blog/categories/"
 POST_ENDPOINT = "http://localhost:8888/api/blog/posts/"
 
-def test_check_new_blog_posts_exist_in_category():
-    # Get all category 3
+def count_posts_in_category(response_body):
+    count = 0
+    for x in response_body:
+        if isinstance(response_body[x], list):
+            count += len(response_body[x])
+    return count
+
+
+def test_check_new_blog_posts_added_to_category():
+    # Get all blog posts in category 3
     before_blog_post = requests.get(GET_ENDPOINT + "3")
     before_blog_post_response_body = before_blog_post.json()
 
-    #Count how many blog posts category has before post
-    before_blog_count = 0
-    for x in before_blog_post_response_body:
-        if isinstance(before_blog_post_response_body[x], list):
-            before_blog_count += len(before_blog_post_response_body[x])
-    print(before_blog_count)
-
+    # Count how many blog posts category 3 has before post
+    before_blog_count = count_posts_in_category(before_blog_post_response_body)
 
     # Set up a new blog post for category 3
     payload = "{\n  \"body\": \"This is a brand new blog post\"," \
@@ -27,16 +30,11 @@ def test_check_new_blog_posts_exist_in_category():
     new_post_response = requests.request("POST", url=POST_ENDPOINT, headers=HEADERS, data=payload)
     assert new_post_response.status_code == 201
 
-    #Count how many blog posts category has after post
+    # Count how many blog posts category 3 has after post
     after_blog_post = requests.get(GET_ENDPOINT + "3")
     after_blog_post_response_body = after_blog_post.json()
 
+    after_blog_count = count_posts_in_category(after_blog_post_response_body)
 
-    after_blog_count = 0
-    for x in after_blog_post_response_body:
-        if isinstance(after_blog_post_response_body[x], list):
-            after_blog_count += len(after_blog_post_response_body[x])
-    print(after_blog_count)
-
-    pytest.set_trace()
+    # Assert that category 3 count has increased by 1
     assert after_blog_count == before_blog_count + 1
